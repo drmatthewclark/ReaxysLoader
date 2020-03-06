864084442db23d74fd3e028105e0f90e2a49e690
Matthew Clark <mclark@drmatthewclark.com>
2020-03-06 13:26:33 -0500

    initialize

---
+++ readrdfiles.py
@@ -0,0 +1,172 @@
+dbname='mclark'
+debug = False
+
+import xml.etree.ElementTree as ET
+import psycopg2 as psql
+from   psycopg2 import sql
+from psycopg2.extensions import AsIs
+import glob
+import gzip
+
+# for rdkit Mol to Smiles
+from rdkit import Chem
+from rdkit import RDLogger
+import rdkit.Chem.rdChemReactions
+
+
+def readnextRDfile(file):
+    line = '' # file.readline()
+    rdfile = ''
+    tags = {}
+    blankcount = 0
+
+    line = file.readline().rstrip()
+    line = ''
+    while not line.startswith('$RFMT $RIREG'):
+        line = file.readline()
+        if not line.startswith('$RFMT $RIREG'):
+            rdfile = rdfile + line
+        # python file reading is brain dead and returns an empty string
+        # at the EOF instad of  None or any other EOF marker so we have to
+        # guess that a bunch of blank lines means that we have reached the
+        # end of file 
+        if line == '':
+            blankcount += 1
+        if blankcount > 2:
+            break
+
+    tags = processRXN(rdfile)
+    return tags # dictionary 
+
+
+global counter 
+counter = 0
+
+def processRXN(rdfile):
+    global counter
+    header = '' + '\n' + 'GSMACCS-II07189510252D 1   0.00366     0.00000     0' + '\n\n'  + '  0  0  0     0  0            999 V3000' + '\n'
+    tail = 'M  END' 
+    counter += 1
+    print("rdfile: ", counter)
+    reactants = list()
+    products = list()
+    currentstructure = header
+    state = ''
+    dtype = None
+    data = {}
+
+    for line in rdfile.splitlines():
+        endctab = None
+        if line.startswith('M  V30 BEGIN REACTANT'): # reactant section
+            state = 'reactant'  # the structures are reactants
+            continue
+        if line.startswith('M  V30 END REACTANT'):
+            state = None   # reactant section has ended
+            continue
+        if line.startswith('M  V30 BEGIN PRODUCT'):
+            state = 'product'  # molecules are now products
+            continue
+        if line.startswith('M  V30 END PRODUCT'):
+            state = None  # product section has ended
+            continue
+        if line.startswith('M  V30 END CTAB'):
+            endctab = True 
+        if state:
+            currentstructure += line + '\n'
+       
+        if endctab:
+            endctab = None
+            if state == 'reactant':
+                currentstructure += tail
+                reactants.append(currentstructure)
+                currentstructure = header
+            elif state == 'product':
+                currentstructure += tail
+                products.append(currentstructure) 
+                currentstructure = header
+            elif state is None:
+                print("error parsing RDfile ")
+
+        if line.startswith('$DTYPE'):
+            dtype = line[12:]
+
+        if dtype and line.startswith('$DATUM'):
+            datum = line[7:]
+
+            if dtype.startswith('RX'):
+                data[dtype] = datum
+            elif dtype.startswith('TRANSFORM'):
+                data[dtype[14:]] = datum
+            dtype = None
+
+    data['rxnsmiles'] = tosmiles(products, reactants)
+    return data
+
+def tosmiles(products, reactants):
+    try:
+       smiles = ''
+       for r in reactants:
+           mol = Chem.MolFromMolBlock(r)
+           smiles += Chem.MolToSmiles(mol) + '.'
+       smiles = smiles[:-1]
+       smiles += '>>'
+       for p in products:
+           mol = Chem.MolFromMolBlock(r)
+           smiles += Chem.MolToSmiles(mol) + '.'
+       smiles = smiles[:-1]
+   
+       rxn = Chem.rdChemReactions.ReactionFromSmarts(smiles)
+       sm = Chem.rdChemReactions.ReactionToSmiles(rxn)
+       return sm
+    except:
+       return None
+
+def readrdfiles(fname):
+    """ read all of the individual SDFiles from the concatenated SDFile """
+    print('readrdfiless', fname)
+    lg = RDLogger.logger()
+    lg.setLevel(RDLogger.CRITICAL)
+    count = 0
+    conn=psql.connect(user=dbname)
+    with gzip.open(fname, 'rt') as file:
+        # remove header lines
+        file.readline()
+        file.readline()
+        # loop over concatenated files
+        while True:
+            rdrecord = readnextRDfile(file)
+            if rdrecord is None:
+                break;
+            writedb(conn, rdrecord)
+            count += 1
+
+    conn.commit()
+    conn.close()
+    print("wrote ", count, " records")
+
+
+
+
+def writedb(conn, data):
+     """ write a SDFile record the database """
+
+     sql = 'insert into reaxys.rdfile (%s) values %s'
+     with conn.cursor() as cur:
+         columns = data.keys()
+         values = [data[column] for column in columns]
+         if debug:
+             print(cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))))
+         else:
+             cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))
+
+
+
+def readrdfile():
+  """ read the SDFiles. This requires special functions because this is
+    not an XML file
+  """
+  for filepath in glob.iglob('rdf/*.rdf.gz'):
+    readrdfiles(filepath)
+    break
+
+readrdfile()
---
+++ readreaxys.py
@@ -0,0 +1,294 @@
+# rmc file release number
+rmcversion = 'rx200171'
+dbname='mclark'
+debug = False
+
+import xml.etree.ElementTree as ET
+import psycopg2 as psql
+from   psycopg2 import sql
+from psycopg2.extensions import AsIs
+import glob
+import gzip
+import hashlib
+
+# for rdkit Mol to Smiles
+#from rdkit import Chem
+#from rdkit import RDLogger
+
+def getid(element):
+    """ returns a hash code for the XML element and children to create a repeatable id for the element """
+    if element is None:
+        return -1
+
+    id = element.attrib.get('ID') 
+    if id and id.isnumeric() and len(list(element)) == 1:
+       return int(id)
+    else:
+       if id is None:
+           id = 'x'
+       return hash(hashlib.md5(id.join(element.itertext()).encode('utf-8')).hexdigest())
+
+def readconditions(fname, dbname):
+    """ 
+    read an xml file into the designated database
+
+    fname - xml file to read
+    key - field for the object key
+    dbname - name for the database to store data to
+    sql - template sql statement to execute for storing the data
+    """
+    print('readconditions', fname)
+    tree = ET.parse(gzip.open(fname));
+    root = tree.getroot()
+    conn = psql.connect(user=dbname)
+    cur = conn.cursor()
+    sql =  'insert into reaxys.conditions (%s) values %s on conflict (condition_id) do nothing';
+    for record in root.findall('REACTIONS/REACTION'):
+        for elem in record.findall('.//CONDITIONS'): 
+            data = {}
+            data['condition_id'] = getid(elem) 
+
+            for condition in ['ATMOSPHERE','PREPARATION','REFLUX']:
+                 subelem = elem.findall(condition)
+                 if subelem:
+                     for sselem in subelem:
+                         tag = sselem.tag
+                         value = sselem.text
+                         data[tag] = value
+    
+            #this set has ranges
+            for condition in ['PH','PRESSURE','REACTION_MOLARITY','TEMPERATURE','TIME','TOTAL_VOLUME']:
+                subelem = elem.find(condition)
+                minmax = {}
+                if subelem:
+                    for subsubelem in subelem:
+                        tag = subsubelem.tag
+                        value = subsubelem.text
+                        if value == '-INF':
+                          value = '-99999999'
+                        elif value == 'INF':
+                          value = '99999999'
+                        minmax[tag] = value
+                
+                    data[condition] = '[' + minmax['min'] + ',' + minmax['max'] + ']'
+ 
+            columns = data.keys()
+            values = [data[column] for column in columns]
+    
+            #print(cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))))       
+            cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))
+    conn.commit()
+    conn.close()
+
+def readstages(fname, dbname):
+    """ 
+    read an xml file into the designated database
+
+    fname - xml file to read
+    key - field for the object key
+    dbname - name for the database to store data to
+    sql - template sql statement to execute for storing the data
+    """
+    print('readstages', fname)
+    tree = ET.parse(gzip.open(fname));
+    root = tree.getroot()
+    conn = psql.connect(user=dbname)
+    cur = conn.cursor()
+    sql =  'insert into reaxys.stages (%s) values %s on conflict (stage_id) do nothing';
+    for record in root.findall('REACTIONS/REACTION'):
+        for elem in record.findall('.//STAGES'): 
+            data = {}
+            data['stage_id'] = getid(elem) 
+    
+            #this set has ranges
+            for item  in ['CONDITIONS', 'REACTANTS', 'PRODUCTS', 'REAGENTS','CATALYSTS','SOLVENTS','METABOLITES']:
+                subelem  = elem.findall(item)
+                if subelem:
+                    ilist = list()
+                    for sselem in subelem:
+                        tag = getid(sselem)
+                        ilist.append(tag)
+                    data[item] = ilist
+ 
+            columns = data.keys()
+            values = [data[column] for column in columns]
+            #print(cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))))       
+            cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))
+    conn.commit()
+    conn.close()
+
+def readvariations(fname, dbname):
+    """ 
+    read an xml file into the designated database
+
+    fname - xml file to read
+    key - field for the object key
+    dbname - name for the database to store data to
+    sql - template sql statement to execute for storing the data
+    """
+    print('readvariations',fname)
+    tree = ET.parse(gzip.open(fname));
+    root = tree.getroot()
+    conn = psql.connect(user=dbname)
+    cur = conn.cursor()
+    sql =  'insert into reaxys.variation (%s) values %s on conflict (variation_id) do nothing';
+    for record in root.findall('REACTIONS/REACTION'):
+        for elem in record.findall('.//VARIATIONS'): 
+            data = {}
+            data['variation_id'] = getid(elem) 
+
+            #this set has lists
+            for item  in ['CREATION_DATE', 'EXPERIMENT_ID','EXPERIMENT_TYPE','MODIFICATION_DATE','PROJECT_NAME','QUALIFICATION',
+                'SOURCE','DESTINATION','CONCLUSION_PHRASE','CREATED_AT_SITE','DUPLICATE_EXP_REF','PREPARATIVE','ELN_CITATION',
+                'REACTION_SCALE','NEXTMOVE_REACTION_TYPE','RXNO_REACTION_TYPE','ANALYTICAL_DATA_EXISTS']:
+                subelem  = elem.find(item)
+                if subelem is not None:
+                    tag = subelem.tag
+                    value = subelem.text
+                    data[tag] = value
+ 
+            for item  in ['STAGES', 'COMMENTS', 'IDENTIFIERS', 'LINKS', 'GROUPS', 'ANIMALS', 'CIT_ID', 'CONDITIONS', 'REACTANTS', 
+                  'PRODUCTS', 'REAGENTS','CATALYSTS','SOLVENTS','METABOLITES','KEYWORDS']:
+                subelem  = elem.findall(item)
+                if subelem:
+                    ilist = list()
+                    for sselem in subelem:
+                        tag = getid(sselem)
+                        ilist.append(tag)
+                    data[item] = ilist 
+
+            columns = data.keys()
+            values = [data[column] for column in columns]
+            #print(cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))))       
+            cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))
+    conn.commit()
+    conn.close()
+
+def readreactions(fname, dbname):
+    """ 
+    read an xml file into the designated database
+
+    fname - xml file to read
+    key - field for the object key
+    dbname - name for the database to store data to
+    sql - template sql statement to execute for storing the data
+    """
+    print('readreactions',fname)
+    tree = ET.parse(gzip.open(fname));
+    root = tree.getroot()
+    conn = psql.connect(user=dbname)
+    cur = conn.cursor()
+    sql =  'insert into reaxys.reaction (%s) values %s on conflict (reaction_id) do nothing';
+    for elem in root.findall('REACTIONS/REACTION'):
+        data = {}
+        data['reaction_id'] = getid(elem) 
+        data['reaxys_reaction_id'] = elem.attrib.get('ID');
+
+        for item in ['RXNSTRUCTURE','RANK','MW_LARGEST_PRODUCT','SORT_CREATION_DATE','SORT_REACTION_SCALE','SORT_TOTAL_VOLUME']:
+            subelem  = elem.find(item)
+            if subelem is not None:
+                tag = subelem.tag
+                value = subelem.text
+                data[tag] = value
+ 
+        for item in ['REACTANT_ID', 'METABOLITE_ID', 'PRODUCT_ID', 'VARIATIONS']:
+            subelem  = elem.findall(item)
+            if subelem:
+                ilist = list()
+                for sselem in subelem:
+                    tag = getid(sselem)
+                    ilist.append(tag)
+                data[item] = ilist 
+
+        columns = data.keys()
+        values = [data[column] for column in columns]
+        #print(cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))))       
+        cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))
+    conn.commit()
+    conn.close()
+
+
+def readsubstances(fname, dbname):
+    """ 
+    read an xml file into the designated database
+
+    fname - xml file to read
+    key - field for the object key
+    dbname - name for the database to store data to
+    sql - template sql statement to execute for storing the data
+    """
+    print('readsubstances', fname)
+    tree = ET.parse(gzip.open(fname));
+    root = tree.getroot()
+    conn = psql.connect(user=dbname)
+    cur = conn.cursor()
+    sql =  'insert into reaxys.substance (%s) values %s on CONFLICT (substance_id) do nothing';
+    for record in root.findall('REACTIONS/REACTION'):
+        for elem in record.findall('.//VARIATIONS'): 
+
+            #this set has lists
+            for item  in ['REACTANTS', 'PRODUCTS', 'REAGENTS','CATALYSTS','SOLVENTS','METABOLITES']:
+                data = {}
+                subelem  = elem.find('.//' + item)
+                if subelem:
+                    data['substance_id'] = getid(subelem)
+                    temp = subelem.attrib.get('ID')
+                    if temp.isnumeric():
+                       data['reaxys_id'] = temp
+                       
+                    for subsubelem in subelem:
+                        tag = subsubelem.tag
+                        value = subsubelem.text
+                        data[tag] = value 
+
+                    columns = data.keys()
+                    values = [data[column] for column in columns]
+                    cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))
+    conn.commit()
+    conn.close() 
+
+
+def readcitations(fname, dbname):
+    """ 
+    read an xml file into the designated database
+
+    fname - xml file to read
+    key - field for the object key
+    dbname - name for the database to store data to
+    sql - template sql statement to execute for storing the data
+    """
+    print('readcitation', fname)
+    tree = ET.parse(gzip.open(fname));
+    root = tree.getroot()
+    conn = psql.connect(user=dbname)
+    cur = conn.cursor()
+    sql =  'insert into reaxys.citation (%s) values %s';
+
+    for elem in root.findall('CITATIONS/CITATION'):
+        data = {}
+        id = elem.attrib.get('ID')
+        data['citation_id'] = id 
+        
+        for subelem in elem:
+            value = subelem.text
+            tag = subelem.tag
+            data[tag] = value
+
+        columns = data.keys()
+        values = [data[column] for column in columns]
+        #print(cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))))       
+        cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))       
+    conn.commit()
+    conn.close()
+
+
+for filepath in glob.iglob('udm-cit/*citations*.xml.gz'):
+  readcitations(filepath, 'mclark')
+
+
+for filepath in glob.iglob('udm-rea/*reactions*.xml.gz'):
+    readreactions(filepath, 'mclark')
+    readconditions(filepath, 'mclark')
+    readstages(filepath, 'mclark')
+    readvariations(filepath, 'mclark')
+    readsubstances(filepath, 'mclark')
