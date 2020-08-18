0d0718ef789e889b314b3a4489602c4fcc7a9adc
Matthew Clark <mclark@drmatthewclark.com>
2020-08-18 08:45:38 -0400

    remove debug data

--- readrdfiles.py
+++ readrdfiles.py
@@ -301,9 +301,7 @@ def readrdfiles():
 
   conn=psql.connect(user=dbname)
 
- # for i, filepath in enumerate(glob.iglob('rdf/*.rdf.gz')):
-  for i, filepath in enumerate('rdf/rx200721_0078.rdf.gz'):
-
+  for i, filepath in enumerate(glob.iglob('rdf/*.rdf.gz')):
         readrdfile(filepath, conn)
         newlen = len(hashset)
         new = newlen - oldlen
