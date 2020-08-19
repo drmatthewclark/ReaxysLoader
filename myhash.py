d7b2df077436b7bd79d8648819fcbe63819c038f
Matthew Clark <mclark@drmatthewclark.com>
2020-08-18 21:37:34 -0400

    return to original final shift for negatives

--- myhash.py
+++ myhash.py
@@ -8,13 +8,13 @@ def myhash(text):
     dint = int(digest,16)  # create 128 bit integer
 
     # fold hi/low bytes into a 64 bit integer.
-    hibytes = ((dint & 0xFFFFFFFFFFFFFFFF0000000000000000) >> 64)
+    hibytes =  (dint & 0xFFFFFFFFFFFFFFFF0000000000000000) >> 64
     lobytes =   dint & 0x0000000000000000FFFFFFFFFFFFFFFF
     # xor high and low halves
     result  = (hibytes ^ lobytes) 
     # if greater than max 64bit signed int, make negative. python doesn't wrap on overflow
     # because it never overflows
     if (result > 0x7FFFFFFFFFFFFFFF):
-        result = -(result >> 1) 
+        result = -(result  >> 1)
 
     return result
