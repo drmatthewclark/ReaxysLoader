832d817a19ce823169bb361551a247f2d7a877d9
Matthew Clark <mclark@drmatthewclark.com>
2021-08-29 11:37:15 -0400

    update with consistent hasher

--- myhash.py
+++ myhash.py
@@ -4,14 +4,11 @@ def myhash(text):
     """ replaces the built in python hash function with one that is deterministic """
  
     # use md5 to create a hash
-    digest  = hashlib.md5(text).hexdigest()
+    digest  = hashlib.md5(text.encode('utf8')).hexdigest()
     dint = int(digest,16)  # create 128 bit integer
 
-    # fold hi/low bytes into a 64 bit integer.
-    hibytes =  (dint & 0xFFFFFFFFFFFFFFFF0000000000000000) >> 64
-    lobytes =   dint & 0x0000000000000000FFFFFFFFFFFFFFFF
     # xor high and low halves
-    result  = (hibytes ^ lobytes) 
+    result  = ((dint  >> 64 ) ^ dint) & 0xFFFFFFFFFFFFFFFF
     # if greater than max 64bit signed int, make negative. python doesn't wrap on overflow
     # because it never overflows
     if (result > 0x7FFFFFFFFFFFFFFF):
