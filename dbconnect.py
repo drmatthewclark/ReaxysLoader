b7c7db7a81ea7efc1e924e850048dd8977d4d72e
Matt Clark <mclark@ip-10-156-21-6.els.vpc.local>
2021-11-05 14:22:58 +0000

    add annotation

--- dbconnect.py
+++ dbconnect.py
@@ -2,6 +2,16 @@ import psycopg2 as psql
 import os
 from subprocess import check_output
 from credentials import *  # import dbase, user, password, host variables
+#
+# create the file credentials.py with these variables:
+# this lets the github allow you to add your own credentials without
+# editing the file from git.
+#
+# host = 'localhost'
+# user = 'rmc'
+# password = ''
+# dbase = 'rmc'
+
 
 def getConnection():
     conn = psql.connect(dbname=dbase, user=user, password=password, host=host)
