a7985a060db5b5e8ced2d9683d7e8b4f03e6c5aa
Matthew Clark <m.clark@elsevier.com>
2021-12-02 15:33:24 +0000

    add removing old versions

--- fullupdate.sh
+++ fullupdate.sh
@@ -12,3 +12,17 @@ eval "$(conda shell.bash hook)"
 conda activate standard
 time python ../ReaxysLoader/load_all.py
 ../../fix-perms
+
+cd ..
+
+del() {
+  shift
+  for d in $*; do
+        echo 'removing dataset' $d
+        rm -r "${d}"
+  done
+
+}
+
+dirs=`ls -dc 2[0-9]*`
+del ${dirs}
